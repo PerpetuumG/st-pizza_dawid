6307c23376afbdd34fccf5ef948e6e3565e79ef4
PerpetuumG <perpetuumg@gmail.com>
2023-12-06 10:43:35 +0200

    add to update userName, avatar

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { User } from '@/models/User';
+
+export const PUT = async req => {
+  await mongoose.connect(process.env.MONGO_URL);
+
+  const data = await req.json();
+  const session = await getServerSession(authOptions);
+  const email = session.user.email;
+
+  const update = {};
+
+  if ('name' in data) {
+    update.name = data.name;
+  }
+
+  if ('image' in data) {
+    update.image = data.image;
+  }
+
+  if (Object.keys(update).length > 0) {
+    // update user name
+    await User.updateOne({ email }, update);
+  }
+
+  return Response.json(true);
+};
---
+++ src/app/profile/page.jsx
@@ -0,0 +1,138 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+const ProfilePage = () => {
+  const session = useSession();
+  const [userName, setUserName] = useState('');
+  const [image, setImage] = useState('');
+  const [saved, setSaved] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const { status } = session;
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      setUserName(session.data.user.name);
+      setImage(session.data.user.image);
+    }
+  }, [session, status]);
+
+  const handleProfileInfoUpdate = async e => {
+    e.preventDefault();
+
+    setSaved(false);
+    setIsSaving(true);
+
+    const response = await fetch('/api/profile', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: userName, image: image }),
+    });
+
+    setIsSaving(false);
+
+    if (response.ok) {
+      setSaved(true);
+    }
+  };
+
+  const handleFileChange = async e => {
+    e.preventDefault();
+
+    const files = e.target.files;
+
+    if (files.length === 1) {
+      const data = new FormData();
+      data.set('file', files[0]);
+
+      setIsUploading(true);
+
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: data,
+      });
+
+      const link = await response.json();
+      setImage(link);
+      setIsUploading(false);
+    }
+  };
+
+  if (status === 'loading') {
+    return 'Loading...';
+  }
+  if (status === 'unauthenticated') {
+    return redirect('/login');
+  }
+
+  return (
+    <section className={'mt-8'}>
+      <h1 className={'text-center text-primary text-4xl mb-4'}>Profile</h1>
+
+      <div className={'max-w-md mx-auto'}>
+        {saved && (
+          <h2 className={'text-center bg-green-100 p-4 rounded-lg border border-green-300'}>
+            Profile saved!
+          </h2>
+        )}
+
+        {isSaving && (
+          <h2 className={'text-center bg-blue-100 p-4 rounded-lg border border-blue-300'}>
+            Saving...
+          </h2>
+        )}
+
+        {isUploading && (
+          <h2 className={'text-center bg-blue-100 p-4 rounded-lg border border-blue-300'}>
+            Uploading...
+          </h2>
+        )}
+
+        <div className={'flex gap-4 items-center'}>
+          <div>
+            <div className={'p-2 rounded-lg relative max-w-[120px]'}>
+              {image && (
+                <Image
+                  className={'rounded-lg w-full h-full mb-1'}
+                  src={image}
+                  alt={'avatar'}
+                  width={250}
+                  height={250}
+                />
+              )}
+
+              <label>
+                <input type='file' className={'hidden'} onChange={handleFileChange} />
+                <span
+                  className={
+                    'block border border-gray-300 rounded-lg p-2 text-center cursor-pointer'
+                  }
+                >
+                  Edit
+                </span>
+              </label>
+            </div>
+          </div>
+
+          <form className={'grow'} onSubmit={handleProfileInfoUpdate}>
+            <input
+              type='text'
+              placeholder={'First and last name'}
+              value={userName}
+              onChange={e => setUserName(e.target.value)}
+            />
+            <input type='email' disabled={true} value={session.data.user.email} />
+
+            <button type={'submit'}>Save</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProfilePage;
